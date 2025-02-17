2b5d74d566d281a472275ccf2c809102b520434a
swetamaurya <mauryasweta062@gmail.com>
2025-02-17 11:20:32 +0530

    correct

--- assets/js/global-header.js
+++ assets/js/global-header.js
@@ -6,15 +6,35 @@
 //   });
 
 
-function loadContent(url, placeholderId) {
+function loadContent(url, placeholderId, callback) {
     fetch(url)
         .then(response => response.text())
         .then(data => {
             document.getElementById(placeholderId).innerHTML = data;
+            if (callback) callback();
         });
 }
 
 // Load sidebar, header, and footer
 loadContent('sidebar.html', 'sidebar-placeholder');
-loadContent('header.html', 'header-placeholder');
+loadContent('header.html', 'header-placeholder',initializeUser);
 loadContent('footer.html', 'footer-placeholder');
+
+function initializeUser() {
+    let userName = document.getElementById('userName');
+    let userEmail = document.getElementById('userEmail');
+
+    if (!userName || !userEmail) {
+        console.error("User elements not found!");
+        return;
+    }
+
+    console.log("User elements found:", userName, userEmail);
+
+
+    const name = localStorage.getItem("name") || "Guest";
+    const email = localStorage.getItem("email") || "guest@example.com";
+
+    userName.innerText = name;
+    userEmail.innerText = email;
+}
